9f3a3878f38ffeb56b0979574a65f63ae1f1c87c
EkeMinusYou <euonymuslke@gmail.com>
2022-10-11 15:00:14 +0900

    start server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+// import {WebClient} from '@slack/web-api'
+import { createEventAdapter } from '@slack/events-api'
+
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET || "";
+// const slackToken = process.env.SLACK_TOKEN || "";
+const slackEvents = createEventAdapter(slackSigningSecret)
+
+const port = 8080
+
+async function main() {
+  // const client = new WebClient(slackToken)
+  const server = await slackEvents.start(port)
+  console.log(server.address())
+}
+
+slackEvents.on("link_shared", event => {
+  console.log(event)
+})
+
+void main()
